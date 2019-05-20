c730df5d51a699f8d6073a58ba36c328c8f22d3a
Chris Hanson <BlondyHanson@gmail.com>
2019-05-20 09:30:13 +0100

    Changed favicon & title on main page

--- pages/index.vue
+++ pages/index.vue
@@ -2,7 +2,7 @@
   <section>
     <section class="hero animated bounceInRight">
       <div class="hero-body">
-        <h1 class="title is-size-1">Chris is a developer</h1>
+        <h1 class="title is-size-1">Chris Hanson</h1>
         <h2 class="subtitle is-size-4">Welcome to my website!</h2>
       </div>
     </section>
