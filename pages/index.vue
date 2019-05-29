f5d20e4babbbb97adda03e3803408e8e182328d7
Chris Hanson <BlondyHanson@gmail.com>
2019-05-29 08:19:09 +0100

    Added current employment to main page

--- pages/index.vue
+++ pages/index.vue
@@ -8,13 +8,15 @@
     </section>
     <div>
       <article class="container has-text-white animated bounceInRight">
-        I am a passionate junior developer with a diploma in Web &amp; App
-        Development from RED Academy, I have a degree in Mathematics from the
-        University of Hertfordshire and a finance background in the medical
-        device industry. I took a gamble when I switched careers, however it has
-        paid off as I found a new love for coding. I have a thirst for
-        knowledge, I am curious about everything and I love to make processes
-        easier.
+        I am a passionate junior developer currently working as a junior backend
+        developer for
+        <a class="link" href="https://sync.money/">sync.money</a> with a diploma
+        in Web &amp; App Development from RED Academy. I have a degree in
+        Mathematics from the University of Hertfordshire and a finance
+        background in the medical device industry. I took a gamble when I
+        switched careers, however it has paid off as I found a new love for
+        coding. I have a thirst for knowledge, I am curious about everything and
+        I love to make processes easier.
       </article>
       <tabs />
     </div>
